fe23cf89d01de60343f8e6567a662d585bdb639e
Ketan Saini <sainiketan99@gmail.com>
2024-10-03 08:57:22 +0530

    Components: Features, Footer, Header, Hero

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="bg-gray-800 text-gray-400 py-6" id="contact">
+    <div class="container mx-auto text-center">
+      <p>&copy; 2024 Dwindle. All rights reserved.</p>
+      <div class="flex justify-center space-x-6 mt-4">
+        <a href="#" class="hover:text-white">
+          <span class="sr-only">Twitter</span>
+          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+            <!-- Twitter Icon SVG -->
+          </svg>
+        </a>
+        <a href="#" class="hover:text-white">
+          <span class="sr-only">Facebook</span>
+          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+            <!-- Facebook Icon SVG -->
+          </svg>
+        </a>
+        <a href="#" class="hover:text-white">
+          <span class="sr-only">Instagram</span>
+          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+            <!-- Instagram Icon SVG -->
+          </svg>
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+};
+</script>
