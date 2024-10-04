a32a7430c57c07d060b7bf6da1a95a7fbce68dbd
Ketan <sainiketan99@gmail.com>
2024-10-04 15:37:35 +0530

    Header, Hero component update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,6 @@
       </h1>
       <nav class="space-x-4">
         <router-link to="/" class="text-gray-600 hover:text-blue-500">Home</router-link>
-        <router-link to="/features" class="text-gray-600 hover:text-blue-500">Features</router-link>
         <router-link to="/login" class="text-gray-600 hover:text-blue-500">Login</router-link>
         <button @click="logout" class="text-gray-600 hover:text-blue-500">Logout</button>
       </nav>
@@ -18,6 +17,7 @@
 
 <script>
 import { supabase } from '../supabase';
+import { store } from '../store';
 
 export default {
   name: 'AppHeader',
@@ -28,7 +28,8 @@ export default {
         console.error('Logout error:', error.message);
         alert('Logout failed: ' + error.message);
       } else {
-        this.$router.push('/login');
+        this.$router.push('/');
+        store.user = null;
       }
     },
   },
