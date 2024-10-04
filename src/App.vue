8838730a9bc727b38c0317e168589345ef71e7d1
Ketan <sainiketan99@gmail.com>
2024-10-04 15:36:37 +0530

    Main router container

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,8 @@
 </template>
 
 <script>
-import { supabase } from './supabase';
 
 export default {
   name: 'App',
-  created() {
-    supabase.auth.onAuthStateChange((event, session) => {
-      if (event === 'SIGNED_IN') {
-        console.log('User signed in:', session.user);
-        // You can update user state or redirect if necessary
-      } else if (event === 'SIGNED_OUT') {
-        console.log('User signed out');
-        // Handle sign-out actions
-      }
-    });
-  },
 };
 </script>
